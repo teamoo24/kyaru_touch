2c8b4f9c6861be24a2a76318ad6d75a749c1525f
teamoo24 <601107w@gmail.com>
2019-12-23 12:07:47 +0900

    タイトル画面編集必要

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // enchant.js本体やクラスをエクスポートする
 enchant();
 
-// スタート画像を追加
-const start = './img/start.png'
-// ゲームオーバー画像を追加
-const end = './img/end.png'
+const ASSETS = {
+	// スタート画像を追加
+	start : './img/start.png',
+	// ゲームオーバー画像を追加
+	end : './img/end.png'
+}
 
 const canvas = {
 	width : 320,
@@ -13,32 +15,36 @@ const canvas = {
 
 const start_s = {
 	x:40,
-	y:260,
-	w:230,
-	h:50
+	y:160,
+	w:320
 }
 
-var Start_Button = enchant.Class.create(enchant.Sprite,{
-	initialize: function(x,y,w,h) {
-		// 「var player = new Sprite(,)」 = 「enchant.Sprite.call(this,,0)」 
-		enchant.Sprite.call(this, w,h);
+// var Start_Button = enchant.Class.create(enchant.Sprite,{
+// 	initialize: function(x,y,w,h) {
+// 		// 「var player = new Sprite(,)」 = 「enchant.Sprite.call(this,,0)」 
+// 		enchant.Sprite.call(this, w,h);
 
-		this.image = game.assets[start]
-		this.x = start_s.x;
-		this.y = start_s.y;
+// 		this.image = game.assets[start]
+// 		this.x = start_s.x;
+// 		this.y = start_s.y;
 
-		this.addEventListener(Event.TOUCH_END, function(e) {
-			game.rootScene.removeChild(this)
-		});
-	}
-});
+// 		this.addEventListener(Event.TOUCH_END, function(e) {
+// 			game.rootScene.removeChild(this)
+// 		});
+// 	}
+// });
 
 window.onload = function() {
 	game = new Core(canvas.width, canvas.heigt)
 	game.fps = 30;
-	game.preload(start, end);
+	game.rootScene.backgroundColor = "#4abafa";
+	game.preload(ASSETS);
 	game.onload = function(){
-		var start_button = new Start_Button(start_s.x,start_s.y,start_s.w,start_s.h);
+		var start_button = new MutableText(start_s.x, start_s.y, start_s.w);
+		start_button.setText("Touch to start")
+		start_button.addEventListener(Event.TOUCH_END,function(e){
+			game.rootScene.removeChild(start_button)
+		});
 		game.rootScene.addChild(start_button);
 	}
 	game.start();
